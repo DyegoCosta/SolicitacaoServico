447a8f10417e6b9bc9acd19cd100a551441170a8
Joberto <jobertodinizjunior@gmail.com>
2012-05-17 20:24:59 -0300

    insert is working for cliente

--- test/ConexaoTeste.java
+++ test/ConexaoTeste.java
@@ -3,26 +3,29 @@ import Domain.Data.DatabaseFactory;
 import Domain.Data.IDatabaseFactory;
 import Domain.Repository.IApontamentoRepository;
 import Domain.Models.Apontamento;
+import Domain.Models.Cliente;
+import Domain.Repository.IClienteRepository;
 import Infrastructure.Repository.ApontamentoRepository;
+import Infrastructure.Repository.ClienteRepository;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ConexaoTeste {
-
+    
     private IDatabaseFactory databaseFactory;
-
+    
     public ConexaoTeste() {
     }
-
+    
     @Before
     public void setUp() {
-
+        
         databaseFactory = new DatabaseFactory("Infrastructure/Configuration/hibernate.cfg.xml");
     }
-
+    
     @Test
-    public void hello() {
+    public void obter() {
         // Arrange
         IApontamentoRepository apontamentoRepository = new ApontamentoRepository(databaseFactory);
         int id = 2;
@@ -32,5 +35,24 @@ public class ConexaoTeste {
 
         // Assert
         assertNotNull(apontamento);
+    }
+    
+    @Test
+    public void insert() {
+        // Arrange
+        IClienteRepository clienteRepository = new ClienteRepository(databaseFactory);
+        Cliente cliente = new Cliente();
+        cliente.setCNPJ(2322323);
+        cliente.setEndereco("rua tal tal");
+        cliente.setNomeResponsavel("Qualquer um");
+        cliente.setRazaoSocial("Razao total pra esse cara");
+        cliente.setTelefone("32352069");
+        cliente.setEmail("joba@joba");
+
+        // Act
+        clienteRepository.salvar(cliente);
+
+        // Assert
+        assertTrue(cliente.getClienteId() != 0);
     }
 }
