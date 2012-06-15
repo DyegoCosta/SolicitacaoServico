40e4fdc3e89f134cedee53ff3e4c5912cf6e42a3
Joberto <jobertodinizjunior@gmail.com>
2012-06-15 19:19:40 -0300

    this closes #13 #11

--- src/Domain/Repository/IClienteRepository.java
+++ src/Domain/Repository/IClienteRepository.java
@@ -1,7 +1,9 @@
 package Domain.Repository;
 
 import Domain.Models.Cliente;
+import java.util.List;
 
 public interface IClienteRepository extends IBaseRepository<Cliente> {
     
+    List<Cliente> listarPorCriterio(String texto);
 }
--- src/Infrastructure/Repository/ClienteRepository.java
+++ src/Infrastructure/Repository/ClienteRepository.java
@@ -3,10 +3,28 @@ package Infrastructure.Repository;
 import Domain.Data.IDatabaseFactory;
 import Domain.Models.Cliente;
 import Domain.Repository.IClienteRepository;
+import java.util.List;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 
 public class ClienteRepository extends BaseRepository<Cliente> implements IClienteRepository {
     
     public ClienteRepository(IDatabaseFactory databaseFactory){
          super(databaseFactory);
+    }
+
+    @Override
+    public List<Cliente> listarPorCriterio(String texto) {
+        
+        texto = adicionarSinalPorcentagem(texto);
+        Criteria criterio = session.createCriteria(Cliente.class)
+                                   .add(Restrictions.disjunction()
+                                                    .add(Restrictions.ilike("razaoSocial", texto))
+                                                    .add(Restrictions.ilike("CNPJ", texto))
+                                                    .add(Restrictions.ilike("nomeResponsavel", texto))
+                                                    .add(Restrictions.ilike("telefone", texto)))
+                                                    .add(Restrictions.ilike("endereco", texto))
+                                                    .add(Restrictions.ilike("email", texto));
+        return criterio.list();
     }
 }
